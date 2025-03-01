b6327fb86f2765b093d6199ff0c85aa4507aa26c
https://github.com/redondo519/PROG_DAVID.git <david.redalo@iesjulianmarias.es>
2025-03-01 20:51:09 +0100

    comit 20:50

--- botones.java
+++ botones.java
@@ -81,7 +81,7 @@ public class botones {
     //funcion mostrar tablero
     public static void mostrarTablero(int [][] tablero){
 
-        System.out.print("-------------");
+        System.out.print("----------------------------------------------------");
         for(int i = 1; i <=6;i++){
             System.out.println();
             for(int j = 1; j <=6;j++){
